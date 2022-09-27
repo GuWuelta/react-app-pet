453f35add31fab92c7bb9ae681871054bf1ecba7
GuWuelta <rm93449@fiap.com.br>
2022-09-27 15:23:02 -0300

    Colors alteradas.

--- src/style/styled.js
+++ src/style/styled.js
@@ -6,19 +6,19 @@ export const DivLista = styled.div`
     width: 100%;
     padding: 20px;
     min-height: 85vh;
-    background-color: #ccc;
+    background-color: #00FA9A;
     display: flex; flex-wrap: wrap;
     justify-content: space-around;
 `
 
 export const DivTarefa = styled.div`
-    background-color: #ffb;
+    background-color: #A020F0;
     border: 2px solid #333;
     box-shadow: 5px 5px 5px #333;
     padding: 20px;
     text-align: center;
     width: 350px;
-    height: 200px;
+    height: 250px;
     margin: 10px;
     h2,p{ padding-bottom: 10px; }
 `
